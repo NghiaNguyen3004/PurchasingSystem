8ce1faa596309521009c289e1a9fd3d6d2e6c8f2
Nghia Nguyen <nghianguyen3004@gmail.com>
2026-06-01 15:42:49 +0700

    Finished the route to get one's requests

--- backend/controllers/requestController.js
+++ backend/controllers/requestController.js
@@ -1,4 +1,4 @@
-import {createRequest} from '../models/requestModel.js'
+import {createRequest, getRequestsByUserId} from '../models/requestModel.js'
 
 export const submitRequest = async (req, res) => {
     const {item_name, quantity, price_per_unit, budget_code, reason} = req.body
@@ -9,6 +9,17 @@ export const submitRequest = async (req, res) => {
         res.status(201).json({ message: 'Request submitted successfully' })
     } catch (error) {
         console.log('Request submission error:', error)
+        res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
+export const getUserRequests = async (req, res) => {
+    const userId = req.user.userId
+    try {
+        const userRequests = await getRequestsByUserId(userId)
+        res.status(200).json({ requests: userRequests })
+    } catch (error) {
+        console.error('Error fetching user requests:', error)
         res.status(500).json({ message: 'Internal server error' })
     }
 }
--- backend/routes/request.js
+++ backend/routes/request.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import {authenticateToken} from '../middleware/authMiddle.js'
-import {submitRequest} from '../controllers/requestController.js'
+import {submitRequest, getUserRequests} from '../controllers/requestController.js'
 
 const requestRouter = express.Router()
 
 requestRouter.post('/', authenticateToken, submitRequest)
-
+requestRouter.get('/mine', authenticateToken, getUserRequests)
 export default requestRouter
